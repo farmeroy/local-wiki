a6422065756e757cdb9cbeba239df1de17c381d5
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-04-17 16:16:29 +0200

    removed classname

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import TopNav from './components/TopNav';
+
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <TopNav />
+      <Routes>
+        
+      </Routes>
     </div>
   );
 }
